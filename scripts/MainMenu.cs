2a452b3bce577d0f658eea6633648ae69de2612a
NucleoFusion <lakshit.singh.mail@gmail.com>
2025-04-06 10:10:35 +0530

    formatting

--- scripts/MainMenu.cs
+++ scripts/MainMenu.cs
@@ -5,33 +5,34 @@ using System;
 
 public partial class MainMenu : Control
 {
-	private Button _start;
-	private Button _quit;
-	private Button _settings;
+    private Button _start;
+    private Button _quit;
+    private Button _settings;
 
-	public override void _Ready()
-	{
-		_start = GetTree().CurrentScene.GetNode<Button>("VBoxContainer/Start");
-		_quit = GetTree().CurrentScene.GetNode<Button>("VBoxContainer/Quit");
-		_settings = GetTree().CurrentScene.GetNode<Button>("VBoxContainer/Settings");
+    public override void _Ready()
+    {
+        _start = GetTree().CurrentScene.GetNode<Button>("VBoxContainer/Start");
+        _quit = GetTree().CurrentScene.GetNode<Button>("VBoxContainer/Quit");
+        _settings = GetTree().CurrentScene.GetNode<Button>("VBoxContainer/Settings");
 
-		_start.Pressed += Start;
-		_quit.Pressed += Quit;
-		_settings.Pressed += Settings;
-	}
+        _start.Pressed += Start;
+        _quit.Pressed += Quit;
+        _settings.Pressed += Settings;
+    }
 
-	public void Start()
-	{
-		GetTree().ChangeSceneToFile("res://scenes/map_selection.tscn");
-	}
+    public void Start()
+    {
+        GetTree().ChangeSceneToFile("res://scenes/map_selection.tscn");
+    }
 
-	public void Quit()
-	{
-		GetTree().Quit();
-	}
-	public void Settings()
-	{
-		// GetTree().ChangeSceneToFile("res://scenes/settings.tscn");
-	}
+    public void Quit()
+    {
+        GetTree().Quit();
+    }
+
+    public void Settings()
+    {
+        GetTree().ChangeSceneToFile("res://scenes/settings.tscn");
+    }
 
 }
